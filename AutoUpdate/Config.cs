9645275523c8ec2d60f5e01ad4a80b36979605a1
DiabeloDev <diabelodev@gmail.com>
2025-06-06 13:22:48 +0200

    Update README.md

--- AutoUpdate/Config.cs
+++ AutoUpdate/Config.cs
@@ -8,7 +8,6 @@ namespace AutoUpdate
     {
         public bool IsEnabled { get; set; } = true;
         public bool Debug { get; set; } = false;
-        [Description("Path to the JSON file containing the list of plugins to update. Format: {\"PluginName\": \"GitHubUser/RepoName\"}")]
         public string RepositoriesConfigPath { get; set; } = $"{Paths.Configs}/AutoUpdate/repositories.json";
 
         [Description("Run updater at start")] 
